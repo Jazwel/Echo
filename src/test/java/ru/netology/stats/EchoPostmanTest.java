cd855cfeb8b2df56c3c1a244c58e54740586343f
DS <mc_sd@bk.ru>
2023-06-22 18:00:36 -0300

    reformat code

--- src/test/java/ru/netology/stats/EchoPostmanTest.java
+++ src/test/java/ru/netology/stats/EchoPostmanTest.java
@@ -7,19 +7,18 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class EchoPostmanTest {
 
-  @Test
-  void CheckTheBody() {
+    @Test
+    void CheckTheBody() {
 
-    given()
-            .baseUri("https://postman-echo.com")
-            .body("echo") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-            .when()
-            .post("/post")
-// Проверки
-            .then()
-            .statusCode(200)
-            .body("data",equalTo("echo"));
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("echo") //
+                .when()
+                .post("/post")
 
-  }
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("echo"));
+
+    }
 }
